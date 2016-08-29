36c506ad4e6fd807eb2b16d9750c435da24d7dad
Richard Janík <rjanik@redhat.com>
2016-08-29 10:50:42 +0200

    Attempt at reproducing JBEAP-5785

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/sso/ClusteredSingleSignOnTestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/sso/ClusteredSingleSignOnTestCase.java
@@ -21,8 +21,6 @@
  */
 package org.jboss.as.test.clustering.cluster.sso;
 
-import java.net.URL;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -32,12 +30,16 @@ import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.test.clustering.cluster.ClusterAbstractTestCase;
+import org.jboss.as.test.integration.web.sso.InvalidationServlet;
 import org.jboss.as.test.integration.web.sso.LogoutServlet;
 import org.jboss.as.test.integration.web.sso.SSOTestBase;
 import org.jboss.logging.Logger;
 import org.jboss.shrinkwrap.api.Archive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URL;
 
 /**
  * @author <a href="mailto:dpospisi@redhat.com">Dominik Pospisil</a>
@@ -117,10 +119,10 @@ public class ClusteredSingleSignOnTestCase extends ClusterAbstractTestCase {
      */
     @Test
     public void testFormAuthSingleSignOn(
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
         log.info("+++ testFormAuthSingleSignOn");
-        SSOTestBase.executeFormAuthSingleSignOnTest(new URL(baseURL1, "/"), new URL(baseURL2, "/"), log);
+        SSOTestBase.executeFormAuthSingleSignOnTestWithInvalidation(new URL(baseURL1, "/"), new URL(baseURL2, "/"), log);
     }
 
     /**
@@ -128,8 +130,8 @@ public class ClusteredSingleSignOnTestCase extends ClusterAbstractTestCase {
      */
     @Test
     public void testNoAuthSingleSignOn(
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
         log.info("+++ testNoAuthSingleSignOn");
         SSOTestBase.executeNoAuthSingleSignOnTest(new URL(baseURL1, "/"), new URL(baseURL2, "/"), log);
     }
@@ -141,10 +143,21 @@ public class ClusteredSingleSignOnTestCase extends ClusterAbstractTestCase {
      */
     @Test
     public void testSessionTimeoutDestroysSSO(
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
-            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
+            @ArquillianResource(InvalidationServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
         log.info("+++ testSessionTimeoutDestroysSSO");
         SSOTestBase.executeFormAuthSSOTimeoutTest(new URL(baseURL1, "/"), new URL(baseURL2, "/"), log);
+    }
+
+    /**
+     * Test single sign-on with logout by calling {@link HttpServletRequest#logout()}
+     */
+    @Test
+    public void testLogoutWithClusteredSSO(
+            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
+            @ArquillianResource(LogoutServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2) throws Exception {
+        log.info("+++ testLogoutWithClusteredSSO");
+        SSOTestBase.executeFormAuthSingleSignOnTestWithLogout(new URL(baseURL1, "/"), new URL(baseURL2, "/"), log);
     }
 
 }
